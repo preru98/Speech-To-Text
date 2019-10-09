0dbe34a5ed6af031651bf6d5a8e0c0a9077b101f
Prerna <prerna1998.ps@gmail.com>
2019-10-09 20:44:37 +0530

    text To Speech-Basic

---
+++ textToSpeech/textToSpeech/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textToSpeech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text;
+            SpeechSynthesizer S = new SpeechSynthesizer();
+            S.SetOutputToDefaultAudioDevice();
+            Console.WriteLine("Enter the text");
+            S.Speak("Enter the text");
+            text=Console.ReadLine();
+            S.Speak(text);
+            Console.Read();
+
+
+        }
+    }
+}
